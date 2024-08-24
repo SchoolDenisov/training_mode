75796257dc375d156f6e304a917bd6bad7d02d4b
Alexander Denisov <school.denisov@gmail.com>
2024-08-24 14:15:35 +0300

    fixed

--- src/first.c
+++ src/first.c
@@ -6,5 +6,6 @@ int main (void) {
     printf("Я простой ");
     printf("компьютер. \n");
     printf("Моей любимой цифрой является %d, так как она первая. \n", num);
+    getchar();
     return 0;
 }
